92e67f9236d5db2f6a19eb720c837ab5ad2432bc
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2024-08-23 10:54:32 +0200

    Add demo accounts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,10 @@
     <x-slot name="title">Welkom bij de homepagina van het ICTS servicepunt contact flow</x-slot>
 <p>Welkom bij de homepagina van de contact flow van het servicepunt. Log in om vragen te beheren of statistieken te exporteren. Of ga naar [UNDEFINED]
 om de contact flow te volgen.</p>
+    <div class="mt-2">
+        <p>Demo accounts:</p>
+        <p>Email: user@user.dev | ww: User1234</p>
+        <p>Email: admin@admin.dev | ww: Admin1234</p>
+    </div>
 </x-layout.servicepunt>
 
