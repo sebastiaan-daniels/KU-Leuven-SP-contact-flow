be97b2fe1ab0333fac2f697c6a0158da219b68fa
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2024-08-26 16:06:24 +0200

    Create basic contact Flow

--- app/Livewire/ContactFlow.php
+++ app/Livewire/ContactFlow.php
@@ -4,12 +4,42 @@ namespace App\Livewire;
 
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use App\Models\Question;
+use App\Models\Contact;
 
 #[Layout('components.layout.contactflow', ['title' => 'Contact Flow', 'description' => 'Contact flow van het Servicepunt',])]
 class ContactFlow extends Component
 {
+    public $loading = 'Even geduld...';
+    public $currentQuestion = 1;
+
+    public function fetchCurrentQuestionById($id)
+    {
+        return Question::find($id);
+    }
+    public function fetchChildrenById($id)
+    {
+        $children = Question::where('parent_id', '=', $id)->get();
+        return $children;
+    }
+    public function updateCurrentQuestion($id)
+    {
+        $this->currentQuestion = $id;
+    }
+
+    public function fetchContactById($id)
+    {
+        return Contact::find($id);
+    }
     public function render()
     {
-        return view('livewire.contact-flow');
+        $question = $this->fetchCurrentQuestionById($this->currentQuestion);
+        $children = $this->fetchChildrenById($this->currentQuestion);
+
+//        if ($question->id === 2) {
+//            dd($children);
+//        }
+
+        return view('livewire.contact-flow', compact('question', 'children'));
     }
 }
--- resources/views/livewire/contact-flow.blade.php
+++ resources/views/livewire/contact-flow.blade.php
@@ -1,3 +1,42 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    {{-- show preloader while fetching data in the background --}}
+    <div class="fixed top-8 left-1/2 -translate-x-1/2 z-50 animate-pulse"
+         wire:loading>
+        <x-icts.preloader class="bg-sky-400 text-white border border-lime-700 shadow-2xl">
+            {{ $loading }}
+        </x-icts.preloader>
+    </div>
+
+    @if($children->isEmpty())
+        @php
+            $contact = $this->fetchContactById($question->contact_id)
+        @endphp
+        <h2>{{$contact->description}}</h2>
+        <h3>{{$contact->name}}</h3>
+        @if(!is_null($contact->email))
+            <h4>Email:</h4>
+            <a href="mailto:{{$contact->email}}" class="underline">{{$contact->email}}</a>
+        @endif
+        @if(!is_null($contact->phone))
+            <h4>Telefoon</h4>
+            <p>{{$contact->phone}}</p>
+        @endif
+        @if(!is_null($contact->website))
+            <h4>Website</h4>
+            <a href="{{$contact->website}}" class="underline">{{$contact->website}}</a>
+        @endif
+    @else
+        <h1>{{$question->name}}</h1>
+        <x-icts.list type="ul" class="list-outside mx-4 text-sm">
+            @foreach($children as $child)
+                <li>
+                    <button
+                        wire:click="updateCurrentQuestion({{ $child->id }})"
+                        class="text-blue-500 hover:underline">
+                        {{ $child->name }}
+                    </button>
+                </li>
+            @endforeach
+        </x-icts.list>
+    @endif
 </div>
